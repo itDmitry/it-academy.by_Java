b928dd7d9b84c9cde1ab3002dcafb54a24dc8c3a
Dmitry Kozel <76488448+itDmitry@users.noreply.github.com>
2021-01-06 12:37:02 +0300

    DZ1_6

--- home_work_1/DZ1_6.java
+++ home_work_1/DZ1_6.java
@@ -1,7 +1,27 @@
 package home_work_1;
 
 public class DZ1_6 {
+    /*
+    6.* Создать СТАТИЧЕСКИЙ метод createPhoneNumber рядом с методом main,
+    данный метод будет принимает один параметр и будет возвращать строку в отформатированном виде.
+    В метод будет передаваться массив из 10 цифр (от 0 до 9).
+    Данные цифры должны быть помещены в строку формата: (XXX) XXX-XXXX.
+    Например передан массив: {1,2,3,4,5,6,7,8,9,0}, возвращаемый результат: (123) 456-7890.
+    (Взято с https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/java).
+    В задании нельзя использовать колекции, стрима.
+     */
+
+    public static String createPhoneNumber(int[] numbers){
+        String phone = "(" + numbers[0] + numbers[1] + numbers[2] + ")" + " " + numbers[3] + numbers[4] + numbers[5] + "-" +
+                numbers[6] + numbers[7] + numbers[8] + numbers[9];
+        return phone;
+    }
+
     public static void main(String[] args) {
+
+        int[] array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        System.out.println((createPhoneNumber(array)));
 
     }
 }
--- home_work_1/DZ1_7.java
+++ home_work_1/DZ1_7.java
@@ -1,6 +1,24 @@
 package home_work_1;
 
 public class DZ1_7 {
+    /*
+    7.** Создать СТАТИЧЕСКИЙ метод String toBinaryString(byte number) и возвращает двоичное представление числа.
+    В методе можно использовать обычную математику которая описана в теории. Теория https://planetcalc.ru/747.
+    Если число отрицательное то выдавать дополнительный код. Пример:
+	7.1 Вводим: 42.  Возвращает: 00101010
+	7.2 Вводим: 15.  Возвращает: 00001111
+	7.3 Вводим: -42. Возвращает: 11010110
+	7.4 Вводим: -15. Возвращает: 11110001
+     */
+    public static void toBinaryString(byte number){
+        if (number >= 0){
+            Integer.toBinaryString(number);
+        }
+        else {
+
+        }
+    }
+
     public static void main(String[] args) {
 
     }
