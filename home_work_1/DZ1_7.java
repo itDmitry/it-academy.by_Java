68298e33aff32b4f16098fdab0fd374678ad3882
Dmitry Kozel <76488448+itDmitry@users.noreply.github.com>
2021-01-06 15:14:07 +0300

    Update DZ1_7.java

--- home_work_1/DZ1_7.java
+++ home_work_1/DZ1_7.java
@@ -12,61 +12,66 @@ public class DZ1_7 {
 	7.3 Вводим: -42. Возвращает: 11010110
 	7.4 Вводим: -15. Возвращает: 11110001
      */
-    public static void toBinaryString(byte number){
+    public static String toBinaryString(byte number){
+        //Если число положительное, то работаем так
         if (number >= 0){
             int [] binaryArray = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
             byte temp;
             for (int i = (binaryArray.length - 1); i > 0 ; i--) {
-                binaryArray[i] = number%2;
-                temp = (byte)(number/2);
+                binaryArray[i] = number % 2;
+                temp = (byte) (number / 2);
                 number = temp;
             }
-            System.out.println(String.format("%d%d%d%d%d%d%d%d", binaryArray[0], binaryArray[1], binaryArray[2],
-                    binaryArray[3], binaryArray[4], binaryArray[5], binaryArray[6], binaryArray[7]));
+            //Выводим что получилось в двоичном представлении
+            String S = String.format("%d%d%d%d%d%d%d%d", binaryArray[0], binaryArray[1], binaryArray[2],
+                    binaryArray[3], binaryArray[4], binaryArray[5], binaryArray[6], binaryArray[7]);
+            return S;
         }
+        //Если число отрицательное, то работаем так
+        //Сначала делаем его по модулю и берем обычное представление
         else {
-            int [] binaryArray = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+            int[] binaryArray = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
             byte temp;
-            number = (byte)Math.abs(number);
-            for (int i = (binaryArray.length - 1); i > 0 ; i--) {
-                binaryArray[i] = number%2;
-                temp = (byte)(number/2);
+            number = (byte) Math.abs(number);
+            for (int i = (binaryArray.length - 1); i > 0; i--) {
+                binaryArray[i] = number % 2;
+                temp = (byte) (number / 2);
                 number = temp;
             }
+            //Потом его инвертируем (обратный код)
             for (int i = 0; i < binaryArray.length; i++) {
-                if (binaryArray[i] == 0){
+                if (binaryArray[i] == 0) {
                     binaryArray[i] = 1;
-                }
-                else {
+                } else {
                     binaryArray[i] = 0;
                 }
             }
+//Добавляем 1 для дополнительного кода.
+            for (int i = (binaryArray.length - 1); i > 0; i--) {
 
-/*            if (binaryArray[binaryArray.length-1] == 0)
-            {
-                binaryArray[binaryArray.length-1] = 1;
-            }
-            else{
-                if (binaryArray[binaryArray.length-2] == 0){
-                    binaryArray[binaryArray.length-2] = 1;
+                if (binaryArray[i] == 1){
+                    binaryArray[i] = 0;
+                    continue;
                 }
-            } */
 
-            System.out.println(String.format("%d%d%d%d%d%d%d%d", binaryArray[0], binaryArray[1], binaryArray[2],
-                    binaryArray[3], binaryArray[4], binaryArray[5], binaryArray[6], binaryArray[7]));
+                if (binaryArray[i] == 0) {
+                    binaryArray[i] = 1;
+                    break;
+                }
+            }
+//Выводим что получилось в двоичном представлении
+                String S = String.format("%d%d%d%d%d%d%d%d", binaryArray[0], binaryArray[1], binaryArray[2],
+                        binaryArray[3], binaryArray[4], binaryArray[5], binaryArray[6], binaryArray[7]);
+                return S;
+            }
         }
-    }
 
     public static void main(String[] args) {
-
-        System.out.print("Ввели = 42, вывели ");
-        toBinaryString((byte)42);
-        System.out.print("Ввели = 15, вывели ");
-        toBinaryString((byte)15);
-        System.out.print("Ввели = -42, вывели ");
-        toBinaryString((byte)-42);
-        System.out.print("Ввели = -15, вывели ");
-        toBinaryString((byte)-15);
+//Проверяем себя
+        System.out.println("Ввели = 42, вывели " + toBinaryString((byte)42) + ", а должны были 00101010");
+        System.out.println("Ввели = 15, вывели " + toBinaryString((byte)42) + ", а должны были 00001111");
+        System.out.println("Ввели = -42, вывели " + toBinaryString((byte)42) + ", а должны были 11010110");
+        System.out.println("Ввели = -15, вывели " + toBinaryString((byte)-15) + ", а должны были 11110001");
 
     }
 }
