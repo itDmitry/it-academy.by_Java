6918258da1a59ebddea2e0ec1b5c7b2ef97f5274
Dmitry Kozel <76488448+itDmitry@users.noreply.github.com>
2021-01-06 12:10:34 +0300

    Update DZ1_4.java

--- home_work_1/DZ1_4.java
+++ home_work_1/DZ1_4.java
@@ -1,7 +1,28 @@
 package home_work_1;
 
 public class DZ1_4 {
+
+    public static boolean sleepIn (boolean weekday, boolean vacation){
+
+        if (!weekday || vacation){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
     public static void main(String[] args) {
 
+        boolean workingDay = true;
+        boolean onVacation = false;
+
+        if (sleepIn(workingDay, onVacation)){
+            System.out.println("Можем спать дальше");
+        }
+        else {
+            System.out.println("Пора идти на работу");
+        }
     }
 }
