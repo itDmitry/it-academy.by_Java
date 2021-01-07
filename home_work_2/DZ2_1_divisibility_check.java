93835605962cec53fbc430dcc760b064c8d9c44a
Dmitry Kozel <76488448+itDmitry@users.noreply.github.com>
2021-01-07 11:16:49 +0300

    DZ2_2_finished

--- home_work_2/DZ2_1_divisibility_check.java
+++
@@ -1,4 +0,0 @@
-package home_work_2;
-
-public class DZ2_1_divisibility_check {
-}
--- home_work_2/DZ2_1_findMiddleOne.java
+++ home_work_2/DZ2_1_findMiddleOne.java
@@ -1,4 +1,18 @@
-package home_work_2;
+/*
+Среди трех чисел найти среднее
+
+Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
+
+Проверить, лежит ли первое число между двумя другими. При этом может быть два случая:
+
+первое больше второго и первое меньше третьего,
+первое меньше второго и первое больше третьего.
+Если ни один из вариантов не вернул истину, значит первое число не среднее. Тогда проверяется, не лежит ли второе число между двумя другими. Это может быть в двух случаях, когда
+
+второе больше первого и меньше третьего,
+второе меньше первого и больше третьего.
+Если эти варианты также не вернули истину, то остается только один вариант - посередине лежит третье число.
+*/package home_work_2;
 
 import java.util.Scanner;
 
@@ -7,11 +21,11 @@ public class DZ2_1_findMiddleOne {
         double a,b,c;
         Scanner keys = new Scanner(System.in);
         System.out.println("Введи последовательно 3 числа из которых будет найдено среднее");
-        a = keys.nextInt();
+        a = keys.nextDouble();
         System.out.println("Вы ввели число " + a + ", введите еще 2 числа");
-        b = keys.nextInt();
+        b = keys.nextDouble();
         System.out.println("Вы ввели число " + b + ", введите еще 1 число");
-        c = keys.nextInt();
+        c = keys.nextDouble();
         System.out.println("Вы ввели число " + c + ", больше ввод не требуется");
 
         System.out.println("Думаем и считаем, не то что тупаки из омона...");
@@ -22,12 +36,18 @@ public class DZ2_1_findMiddleOne {
     public static double findMiddleOne(double a, double b, double c){
         double middle;
 
-        if ((a <= b && a >= c)||(a >= b && a <= c)){
-            middle = a;
-        } else if ((b <= a && b >= c)||(b >= a && b <= c)){
-            middle = b;
-        } else {
-            middle = c;
+        if ((a == b) || (a == c) || (b == c)){
+            System.out.println("Есть повторяющиеся числа! Перезапусти программу и не будь тупым омоновцем!");
+            middle = 0;
+        }
+        else {
+            if ((a < b && a > c)||(a > b && a < c)){
+                middle = a;
+            } else if ((b < a && b > c)||(b > a && b < c)){
+                middle = b;
+            } else {
+                middle = c;
+            }
         }
 
         return middle;
