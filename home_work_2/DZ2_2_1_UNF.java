bb0bc948961541a16e5aa93b50356cb8cba431d5
Dmitry Kozel <76488448+itDmitry@users.noreply.github.com>
2021-01-12 14:13:16 +0300

    Create DZ2_2_1_UNF.java

---
+++ home_work_2/DZ2_2_1_UNF.java
@@ -0,0 +1,33 @@
+/*
+2.1 Написать метод public static int[] arrayFromConsole() рядом с main.
+Данный метод размер массива и каждый его элемент запрашивает у пользователя через консоль.
+Пример int[] container = arrayFromConsole().
+Результат: В методе arrayFromConsole будет запрошена информация у пользователя,
+пользователь вводит размер (5) и его элементы по порядку {4, 17, 32}.
+Соответсвенно будет создан массив размером 5 с элементами {4, 17, 32} и сохранён в переменную container.
+*/
+package home_work_2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DZ2_2_1_UNF {
+    public static void main(String[] args) {
+        int[] container = arrayFromConsole();
+
+        System.out.println("Получился массив " + Arrays.toString(container));
+    }
+    public static int[] arrayFromConsole(){
+        Scanner console = new Scanner (System.in);
+        System.out.println("Голосуйте за размер массива");
+
+        int [] array = new int[console.nextInt()];
+
+        System.out.println("Голосуйте за элементы массива");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = console.nextInt();
+        }
+
+        return array;
+    }
+}
